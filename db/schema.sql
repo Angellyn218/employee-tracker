d6b836445851ec022cee42b8bd75ea00c28e251c
Angellyn218 <angellyncervantes@yahoo.com>
2022-10-31 16:59:25 -0700

    Fixed employee table

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,8 @@ USE employee_db;
 DROP TABLE IF EXISTS departments;
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL,
+  UNIQUE(name)
 );
 
 DROP TABLE IF EXISTS roles;
@@ -23,10 +24,13 @@ CREATE TABLE roles (
 DROP TABLE IF EXISTS employees;
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
     FOREIGN KEY(department_id)
     REFERENCES departments(id)
-    ON DELETE SET NULL
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
 );
